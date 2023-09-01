96ab08a3b004fd9c34c867755b7ca923c1bc5f90
NadaKhaled157 <nadakhaled9277@gmail.com>
2023-09-02 01:02:32 +0300

    Course Details Page

---
+++ Code/All-Courses.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/homepage.css">
+    <title>All Courses</title>
+</head>
+<body>
+<?php include 'nav-bar.php';?>
+<h1 class="m-3">Enroll Now and kickstart your career!</h1>
+<div class="row mx-auto">
+    <div class="card d-inline-block mx-1 my-2 col-sm-6" style="width: 17.5rem;">
+        <img src="../Photos/Courses/autocad.jpg" class="card-img-top" alt="Robotics">
+        <div class="card-body">
+            <h5 class="card-title">AUTOCAD</h5>
+            <p class="card-text">Do you like tinkering with hardware parts? Check out our robotics workshop!
+                Includes a guided final project. </p>
+            <a href="#" class="btn btn-vintage">Enroll Now</a>
+        </div>
+    </div>
+    <div class="card d-inline-block mx-1 my-2 col-sm-6" style="width: 17.5rem;">
+        <img src="../Photos/Courses/automotive.jpg" class="card-img-top" alt="Web Development">
+        <div class="card-body">
+            <h5 class="card-title">AUTOMOTIVE</h5>
+            <p class="card-text">Kickstart your web development career now! Learn HTML, CSS, Javascript with our finest instructors.</p>
+            <a href="#" class="btn btn-vintage">Enroll Now</a>
+        </div>
+    </div>
+    <div class="card d-inline-block mx-1 my-2 col-sm-6" style="width: 17.5rem;">
+        <img src="../Photos/Courses/c-programming.jpg" class="card-img-top" alt="Web Development">
+        <div class="card-body">
+            <h5 class="card-title">C PROGRAMMING</h5>
+            <p class="card-text">Kickstart your web development career now! Learn HTML, CSS, Javascript with our finest instructors.</p>
+            <a href="#" class="btn btn-vintage">Enroll Now</a>
+        </div>
+    </div>
+    <div class="card d-inline-block mx-1 my-2 col-sm-6" style="width: 17.5rem;">
+        <img src="../Photos/Courses/c++.jpg" class="card-img-top" alt="Web Development">
+        <div class="card-body">
+            <h5 class="card-title">C++</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-vintage">Enroll Now</a>
+        </div>
+    </div>
+    <div class="card d-inline-block mx-1 my-2 col-sm-6" style="width: 17.5rem;">
+        <img src="../Photos/Courses/c++&oop.jpg" class="card-img-top" alt="Web Development">
+        <div class="card-body">
+            <h5 class="card-title">C++ & OOP</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-vintage">Enroll Now</a>
+        </div>
+    </div>
+</div>
+
+<div class="row mx-auto">
+    <div class="card d-inline-block mx-1 my-2 col-sm-6" style="width: 17.5rem;">
+        <img src="../Photos/Courses/matlab.jpg" class="card-img-top" alt="Robotics">
+        <div class="card-body">
+            <h5 class="card-title">MATLAB</h5>
+            <p class="card-text">Do you like tinkering with hardware parts? Check out our robotics workshop!
+                Includes a guided final project. </p>
+            <a href="#" class="btn btn-vintage">Enroll Now</a>
+        </div>
+    </div>
+    <div class="card d-inline-block mx-1 my-2 col-sm-6" style="width: 17.5rem;">
+        <img src="../Photos/Courses/photoshop.jpg" class="card-img-top" alt="Web Development">
+        <div class="card-body">
+            <h5 class="card-title">PHOTOSHOP</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-vintage">Enroll Now</a>
+        </div>
+    </div>
+    <div class="card d-inline-block mx-1 my-2 col-sm-6" style="width: 17.5rem;">
+        <img src="../Photos/Courses/python.jpg" class="card-img-top" alt="Web Development">
+        <div class="card-body">
+            <h5 class="card-title">PYTHON</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-vintage">Enroll Now</a>
+        </div>
+    </div>
+    <div class="card d-inline-block mx-1 my-2 col-sm-6" style="width: 17.5rem;">
+        <img src="../Photos/Courses/revit.jpg" class="card-img-top" alt="Robotics">
+        <div class="card-body">
+            <h5 class="card-title">REVIT</h5>
+            <p class="card-text">Do you like tinkering with hardware parts? Check out our robotics workshop!
+                Includes a guided final project. </p>
+            <a href="#" class="btn btn-vintage">Enroll Now</a>
+        </div>
+    </div>
+    <div class="card d-inline-block mx-1 my-2 col-sm-6" style="width: 17.5rem;">
+        <img src="../Photos/Courses/robotics.jpg" class="card-img-top" alt="Web Development">
+        <div class="card-body">
+            <h5 class="card-title">ROBOTICS</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-vintage">Enroll Now</a>
+        </div>
+    </div>
+</div>
+
+<div class="row mx-auto">
+    <div class="card d-inline-block mx-1 my-2 col-sm-6" style="width: 17.5rem;">
+        <img src="../Photos/Courses/solidworks.jpg" class="card-img-top" alt="Web Development">
+        <div class="card-body">
+            <h5 class="card-title">SOLIDWORKS</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-vintage">Enroll Now</a>
+        </div>
+    </div>
+    <div class="card d-inline-block mx-1 my-2 col-sm-6" style="width: 17.5rem;">
+        <img src="../Photos/Courses/web-dev.jpg" class="card-img-top" alt="Web Development">
+        <div class="card-body">
+            <h5 class="card-title">WEB DEVELOPMENT</h5>
+            <p class="card-text">Kickstart your web development career now! Learn HTML, CSS, Javascript with our finest instructors.</p>
+            <a href="#" class="btn btn-vintage">Enroll Now</a>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
---
+++ Code/Course-Details.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/homepage.css">
+    <!-- Add icon library -->
+<!--    <link rel="stylesheet" href="css/fontawesome.min.css">-->
+    <title id="title">Course Title</title>
+</head>
+<body>
+<script src="js/course-details.js"></script>
+<?php include 'nav-bar.php';?>
+<!--<div class="" style="background-color: #faf1d7; height: 100vh;">-->
+<div style="height: 200px; background-color: #1c1f4c;">
+    <div class="d-lg-flex justify-content-end d-sm-none">
+        <div class="card ms-auto my-3 col-sm-6 position-fixed" style="width: 20rem; margin-right: 100px;">
+            <img src="../Photos/Courses/robotics.jpg" class="card-img-top" alt="Robotics">
+            <div class="card-body">
+                <h5 class="card-title">COURSE TITLE</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <p class="card-text">Course Overview:</p>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">⏳ 12 Sessions</li>
+                    <li class="list-group-item">💻 2 Project Discussions</li>
+                    <li class="list-group-item">💰 40 EGP/session</li>
+                </ul>
+                <a href="#" class="btn btn-vintage d-block">Enroll Now</a>
+            </div>
+        </div>
+    </div>
+    <h1 class="text mx-5 px-5 pt-3">Course Name</h1>
+    <div class="d-inline-block">
+    <p class="text ms-5 ps-5 d-inline-block" style="width: 550px;">Brief Course Description. Maximum two sentences.</p>
+</div>
+    <div class="rating px-5 mx-5">
+        <div id="parent" class="d-inline">
+        <p class="text d-inline">4.5</p>
+            <div class="arrow-box" id="hover-content">
+                REVIEWS
+                <br>
+                userName ******
+                <br>
+                written review.
+                <br>
+                userName ******
+                <br>
+                written review.
+                <br>
+                userName ******
+                <br>
+                written review.
+                <br>
+            </div>
+        </div>
+        <input type="radio" id="star5" name="rating" value="5" /><label for="star5"></label>
+        <input type="radio" id="star4" name="rating" value="4" /><label for="star4"></label>
+        <input type="radio" id="star3" name="rating" value="3" /><label for="star3"></label>
+        <input type="radio" id="star2" name="rating" value="2" /><label for="star2"></label>
+        <input type="radio" id="star1" name="rating" value="1" /><label for="star1"></label>
+    </div>
+
+</div>
+<div class="d-flex flex-wrap" style="width:800px;">
+<div class="card ms-sm-3 ms-md-5 mt-4" style="width:20rem; background-color: #faf1d7">
+    <div class="card-body">
+        <h5 class="card-title fw-bold" style="color:#1c1f4c;">What You Will Learn:</h5>
+        <ul class="list-group list-group-flush" style="background-color: #faf1d7">
+            <li class="list-group-item" style="background-color: #faf1d7; color:#1c1f4c;">Embedded Systems, IC, MPU & MCU Definitions</li>
+            <li class="list-group-item" style="background-color: #faf1d7; color:#1c1f4c;">Configuring Hardware Components</li>
+            <li class="list-group-item" style="background-color: #faf1d7; color:#1c1f4c;">Introduction to Machine Design</li>
+        </ul>
+    </div>
+</div>
+<div class="card ms-sm-3 ms-md-5 mt-4" style="width:20rem; background-color: #00848c">
+    <div class="card-body">
+        <h5 class="card-title fw-bold text-white">Who Are The Instructors?</h5>
+        <p class="text-white">Our instructors are engineers from the top universities in Egypt.</p>
+    </div>
+</div>
+<div class="card ms-sm-3 ms-md-5 my-3" style="width:20rem; background-color: #00848c">
+    <div class="card-body">
+        <h5 class="card-title fw-bold text-white">Bonus Features:</h5>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item text-white" style="background-color: #00848c">Craft a Professional CV</li>
+            <li class="list-group-item text-white" style="background-color: #00848c">Build Your LinkedIn Profile</li>
+            <li class="list-group-item text-white" style="background-color: #00848c">Improve Your Interview Skills</li>
+        </ul>
+    </div>
+</div>
+    <div class="card ms-sm-3 ms-md-5 my-3" style="width:20rem; background-color: #faf1d7">
+        <div class="card-body">
+            <h5 class="card-title fw-bold" style="color:#1c1f4c">Leave a Review!</h5>
+            <div class="rating">
+                <input type="radio" id="star5" name="rating" value="5" /><label for="star5"></label>
+                <input type="radio" id="star4" name="rating" value="4" /><label for="star4"></label>
+                <input type="radio" id="star3" name="rating" value="3" /><label for="star3"></label>
+                <input type="radio" id="star2" name="rating" value="2" /><label for="star2"></label>
+                <input type="radio" id="star1" name="rating" value="1" /><label for="star1"></label>
+            </div>
+
+            <label>
+                <textarea class="form-control" rows="3" cols="50"></textarea>
+            </label>
+                <button type="submit" class="btn btn-vintage mt-2 d-inline" style="margin-left:210px;">Submit</button>
+        </div>
+    </div>
+</div>
+<!--</div>-->
+</body>
+</html>
