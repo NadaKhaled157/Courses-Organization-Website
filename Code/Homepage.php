c34bfe7972b994e51993965dc3f6b20be2a5ccd1
NadaKhaled157 <nadakhaled9277@gmail.com>
2023-09-02 01:01:52 +0300

    Course Details Page

--- Code/Homepage.php
+++ Code/Homepage.php
@@ -6,84 +6,76 @@
     <title>Home Page</title>
 </head>
 <body>
+<?php include 'nav-bar.php';?>
+<div id="carousel" class="carousel" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    </div>
+    <div class="carousel-inner">
+        <div class="carousel-item active col-lg-3">
+            <img id="carousel-image1" src="../Photos/carousel1.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-md-block">
+                <h5>Become a Full Stack Developer</h5>
+                <p>Enroll in our web development workshop now to learn HTML, CSS, and Javascript.</p>
+            </div>
+        </div>
+        <div class="carousel-item col-lg-3">
+            <img src="../Photos/carousel2.jpg" class="d-block w-100" alt="...">
+            <div id="carousel-image2" class="carousel-caption d-md-block">
+                <h5><a href="Signup.html">Signup Now</a></h5>
+                <p>Get 10% off your first course when you sign up to our website.</p>
+            </div>
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+<h1 class="m-3">Browse Our Most Popular Courses!</h1>
+<div class="row mx-auto">
+    <div class="card d-inline-block mx-auto my-2 col-sm-6" style="width: 18rem;">
+        <img src="../Photos/Courses/robotics.jpg" class="card-img-top" alt="Robotics">
+        <div class="card-body">
+            <h5 class="card-title">ROBOTICS</h5>
+            <p class="card-text">Do you like tinkering with hardware parts? Check out our robotics workshop!
+                Includes a guided final project. </p>
+            <a href="#" class="btn btn-vintage">Enroll Now</a>
+        </div>
+    </div>
+    <div class="card d-inline-block mx-auto my-2 col-sm-6" style="width: 18rem;">
+        <img src="../Photos/Courses/web-dev.jpg" class="card-img-top" alt="Web Development">
+        <div class="card-body">
+            <h5 class="card-title">WEB DEVELOPMENT</h5>
+            <p class="card-text">Kickstart your web development career now! Learn HTML, CSS, Javascript with our finest instructors.</p>
+            <a href="#" class="btn btn-vintage">Enroll Now</a>
+        </div>
+    </div>
+    <div class="card d-inline-block mx-auto my-2 col-sm-6" style="width: 18rem;">
+        <img src="../Photos/Courses/python.jpg" class="card-img-top" alt="Python">
+        <div class="card-body">
+            <h5 class="card-title">PYTHON</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-vintage">Enroll Now</a>
+        </div>
+    </div>
+    <div class="card d-inline-block mx-auto my-2 col-sm-6" style="width: 18rem;">
+        <img src="../Photos/Courses/matlab.jpg" class="card-img-top" alt="Matlab">
+        <div class="card-body">
+            <h5 class="card-title">MATLAB</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-vintage">Enroll Now</a>
+        </div>
+    </div>
+</div>
+
+<script src="js/bootstrap.bundle.min.js"></script>
+<!--<script src="js/course-details.js"></script>-->
+<!--<script async src='/browser-sync/browser-sync-client.2.29.3.js'></script>-->
 </body>
 </html>
-<!--<div id="carousel" class="carousel mt-3" data-bs-ride="carousel">-->
-<!--    <div class="carousel-indicators">-->
-<!--        <button type="button" data-bs-target="#carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>-->
-<!--        <button type="button" data-bs-target="#carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>-->
-<!--        <button type="button" data-bs-target="#carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>-->
-<!--    </div>-->
-<!--    <div class="carousel-inner">-->
-<!--        <div class="carousel-item active">-->
-<!--            <img id="carousel-image1" src="../Photos/carousel1.jpg" class="d-block w-100" alt="...">-->
-<!--            <div class="carousel-caption d-none d-md-block">-->
-<!--                <h5>Become a Full Stack Developer</h5>-->
-<!--                <p>Enroll in our web development workshop now to learn HTML, CSS, and Javascript.</p>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--        <div class="carousel-item">-->
-<!--            <img id="carousel-image2" src="../Photos/carousel2.jpg" class="d-block w-100" alt="...">-->
-<!--            <div class="carousel-caption d-none d-md-block">-->
-<!--                <h5>Automotive Workshop</h5>-->
-<!--                <p>Are you a mechanical engineer? Enroll to our Automotive Workshop now!</p>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--        <div class="carousel-item">-->
-<!--            <img src="../Photos/carousel3.jpg" class="d-block w-100" alt="...">-->
-<!--            <div id="carousel-image3" class="carousel-caption d-none d-md-block">-->
-<!--                <h5><a href="Signup.html">Signup Now</a></h5>-->
-<!--                <p>Get 10% off your first course when you sign up to our website.</p>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--    <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">-->
-<!--        <span class="carousel-control-prev-icon" aria-hidden="true"></span>-->
-<!--        <span class="visually-hidden">Previous</span>-->
-<!--    </button>-->
-<!--    <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">-->
-<!--        <span class="carousel-control-next-icon" aria-hidden="true"></span>-->
-<!--        <span class="visually-hidden">Next</span>-->
-<!--    </button>-->
-<!--</div>-->
-<!--<h1 class="m-3">Browse Our Most Popular Courses!</h1>-->
-<!--<div class="row mx-auto">-->
-<!--    <div class="card d-inline-block mx-auto my-2 col-sm-6" style="width: 18rem;">-->
-<!--        <img src="../Photos/robotics.jpg" class="card-img-top" alt="Robotics">-->
-<!--        <div class="card-body">-->
-<!--            <h5 class="card-title">ROBOTICS</h5>-->
-<!--            <p class="card-text">Do you like tinkering with hardware parts? Check out our robotics workshop!-->
-<!--                Includes a guided final project. </p>-->
-<!--            <a href="#" class="btn btn-vintage">Enroll Now</a>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--    <div class="card d-inline-block mx-auto my-2 col-sm-6" style="width: 18rem;">-->
-<!--        <img src="../Photos/web-dev.jpg" class="card-img-top" alt="Web Development">-->
-<!--        <div class="card-body">-->
-<!--            <h5 class="card-title">WEB DEVELOPMENT</h5>-->
-<!--            <p class="card-text">Kickstart your web development career now! Learn HTML, CSS, Javascript with our finest instructors.</p>-->
-<!--            <a href="#" class="btn btn-vintage">Enroll Now</a>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--    <div class="card d-inline-block mx-auto my-2 col-sm-6" style="width: 18rem;">-->
-<!--        <img src="../Photos/python.jpg" class="card-img-top" alt="Web Development">-->
-<!--        <div class="card-body">-->
-<!--            <h5 class="card-title">PYTHON</h5>-->
-<!--            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
-<!--            <a href="#" class="btn btn-vintage">Enroll Now</a>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--    <div class="card d-inline-block mx-auto my-2 col-sm-6" style="width: 18rem;">-->
-<!--        <img src="../Photos/c-programming.jpg" class="card-img-top" alt="Web Development">-->
-<!--        <div class="card-body">-->
-<!--            <h5 class="card-title">C PROGRAMMING</h5>-->
-<!--            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
-<!--            <a href="#" class="btn btn-vintage">Enroll Now</a>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--</div>-->
-<!---->
-<!--<script src="js/bootstrap.bundle.min.js"></script>-->
-<!--<script async src='/browser-sync/browser-sync-client.2.29.3.js'></script>-->
-<!--</body>-->
-<!--</html>-->
---
+++ Code/nav-bar.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/homepage.css">
+    <title>Title</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="Homepage.php">
+            <img src="../Photos/Logo.png" alt="Beta Academy" width="45">
+            Beta Academy
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="Homepage.php">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Categories
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Web Development</a></li>
+                        <li><a class="dropdown-item" href="#">C++</a></li>
+                        <li><a class="dropdown-item" href="#">C++/OOP</a></li>
+                        <li><a class="dropdown-item" href="#">Matlab</a></li>
+                        <li><a class="dropdown-item" href="#">Robotics</a></li>
+                        <li><a class="dropdown-item" href="#">Autocad</a></li>
+                        <li><a class="dropdown-item" href="#">Solidworks</a></li>
+                        <li><a class="dropdown-item" href="#">Automotive</a></li>
+                        <li><a class="dropdown-item" href="#">Revit</a></li>
+                        <li><a class="dropdown-item" href="#">Photoshop</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="All-Courses.php">See All</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="Login.html" tabindex="-1" aria-disabled="false">Login or Signup</a>
+                </li>
+            </ul>
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-vintage" id="button" type="submit">Search</button>
+            </form>
+        </div>
+    </div>
+</nav>
+</body>
+</html>
